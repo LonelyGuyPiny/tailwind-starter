7cedbfb50601b014ded644f616254aa56255e127
Retromac <konitopoulos@hellenictechnologies.com>
2023-06-26 17:19:08 +0300

    tailwind classes optimize / app.css tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.js",
-    "./src/**/*.vue",
-    "./*.html",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  theme: {
-    fontFamily: {
-      'sans': ['SoleilRegular', 'Arial', 'sans-serif'],
-      'book': ['SoleiBook', 'Arial', 'sans-serif'],
-      'bold': ['SoleilBold', 'Arial', 'sans-serif'],
-      'light': ['SoleilLight', 'Arial', 'sans-serif'],
-    },
-    colors: {
-      white: '#FFFFFF',
-      black: '#020F2B',
-      purple: '#B7BDFF',
-      blue: '#4A59FF',
-      darkblue: '#020746',
-      darkgray: '#212121',
-    },
-    extend: {
-      backgroundImage: {
-        'blue-gradient': 'linear-gradient(210deg, #00075A, #020F2B, #00075A, #4A59FF)'
-      },
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "xxl": "1536px"
-      },
-      fontSize: {
-        tiny: [".938rem", "1rem"],
-        xs: [".75rem", "1.2rem"],
-        sm: [".875rem", "1.2rem"],
-        base: ["1rem", "1.5rem"], //16px-24px
-        md: ["1.625rem", "2.375rem"], //26px-38px
-        lg: ["1.875rem", "2.75rem"], //30px-44px
-        xl: ["2.625rem", "3.375rem"], //42px-54px
-        "xxl": ["3.875rem", "4.75rem"], //62px-76px
-      },
-    },
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.js", "./src/**/*.vue", "./*.html", "./node_modules/flowbite/**/*.js"],
+	theme: {
+		fontFamily: {
+			sans: ["SoleilRegular", "Arial", "sans-serif"],
+			book: ["SoleiBook", "Arial", "sans-serif"],
+			bold: ["SoleilBold", "Arial", "sans-serif"],
+			light: ["SoleilLight", "Arial", "sans-serif"]
+		},
+		colors: {
+			white: "#FFFFFF",
+			black: "#020F2B",
+			purple: "#B7BDFF",
+			blue: "#4A59FF",
+			darkblue: "#020746",
+			darkgray: "#212121"
+		},
+		extend: {
+			screens: {
+				xs: "450px",
+				sm: "640px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1280px",
+				"2xl": "1536px"
+			},
+			fontSize: {
+				tiny: [".938rem", "1rem"],
+				xs: [".75rem", "1.2rem"],
+				sm: [".875rem", "1.2rem"],
+				base: ["1rem", "1.5rem"], //16px-24px
+				md: ["1.625rem", "2.375rem"], //26px-38px
+				lg: ["1.875rem", "2.75rem"], //30px-44px
+				xl: ["2.625rem", "3.375rem"], //42px-54px
+				xxl: ["3.875rem", "4.75rem"] //62px-76px
+			}
+		}
+	},
+	plugins: []
+};
